446a7b08576f6132057e75e7df107d4a206fdc00
Your Name <you@example.com>
2019-05-31 13:13:59 -0600

    Initial Commit

---
+++ finalprojctworkpls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalprojctworkpls
+//
+//  Created by Nicholas Spoletini on 2019-05-31.
+//  Copyright © 2019 Nicholas S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
